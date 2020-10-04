13589480b76f0226f3ad91c4135b0d98a0c00aa1
AhmedHenna <AhmedHenna@users.noreply.github.com>
2020-10-04 10:23:23 +0200

    Added Junit Testing
    Added all the testing method for all the method in the Calculation Class

--- Test/UnitTest.java
+++ Test/UnitTest.java
@@ -1,2 +1,31 @@
+import com.company.Calculation;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class UnitTest {
+
+    @Test
+    public void testAddition(){
+        UnitTest first = new UnitTest();
+        assertEquals(9, Calculation.addition(5, 4));
+    }
+    @Test
+    public void testSubtraction(){
+        UnitTest second = new UnitTest();
+        assertEquals(1, Calculation.subtraction(5, 4));
+    }
+
+    @Test
+    public void testDivision(){
+        UnitTest third = new UnitTest();
+        assertEquals(5, Calculation.division(10, 2));
+    }
+
+    @Test
+    public void testMultiplication(){
+        UnitTest fourth = new UnitTest();
+        assertEquals(20, Calculation.multiplication(5, 4));
+    }
+
+
 }
--- src/com/company/Calculation.java
+++ src/com/company/Calculation.java
@@ -2,21 +2,21 @@ package com.company;
 
 public class Calculation {
 
-    public int addition(int a, int b){
+    public static int addition(int a, int b){
         int sum = a + b;
         return sum;
     }
 
-    public int subtraction(int a, int b) {
+    public static int subtraction(int a, int b) {
         int difference = a - b;
         return difference;
     }
-    public int multiplication(int a, int b) {
+    public static int multiplication(int a, int b) {
         int mult = a * b;
         return mult;
     }
 
-    public int division(int a, int b) {
+    public static int division(int a, int b) {
         int div = a / b;
         return div;
     }
