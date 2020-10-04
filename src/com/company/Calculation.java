bc8280175901b44c008b5cfc35cf64433472ff8c
AhmedHenna <AhmedHenna@users.noreply.github.com>
2020-10-04 10:12:25 +0200

    Created Calculation Class and Test Directory
    Made the function of calculator and the tester directory

---
+++ src/com/company/Calculation.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Calculation {
+
+    public int addition(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+    public int subtraction(int a, int b) {
+        int difference = a - b;
+        return difference;
+    }
+    public int multiplication(int a, int b) {
+        int mult = a * b;
+        return mult;
+    }
+
+    public int division(int a, int b) {
+        int div = a / b;
+        return div;
+    }
+}
